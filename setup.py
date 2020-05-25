eae2a7cf5353ad5ad17bb76b55a3c3df526411fe
xnejed07 <xnejed07@gmail.com>
2020-05-25 12:39:06 -0500

    commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyneuroutils',
-    version='1.0.1',
+    version='1.0.2',
     packages=['pyneuroutils'],
     url='https://github.com/xnejed07/pyneuroutils',
     license='MIT',
