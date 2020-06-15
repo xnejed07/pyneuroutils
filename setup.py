9684effa93ddebe1fc3f184c4373931f8650b0f3
xnejed07 <xnejed07@gmail.com>
2020-06-15 12:09:18 -0500

    commit

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup,find_packages
 
 setup(
     name='pyneuroutils',
-    version='1.0.7',
-    packages=['pyneuroutils.statistics'],
+    version='1.0.8',
+    packages=['pyneuroutils.statistics','pyneuroutils.transforms','pyneuroutils.utilities'],
     url='https://github.com/xnejed07/pyneuroutils',
     license='MIT',
     author='Petr Nejedly',
